ca2524c0421a7ac9bcc4895c83f3bb9f4f5d512c
newpageone <liu6email@163.com>
2020-01-20 17:01:38 +0800

    今日code
    code

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,1 +1,22 @@
-Page({})
+Page({
+  data: {
+    name: '张三',
+    counter: 0,
+    students: [
+      { id: 2003, age: 18, name: '张三' },
+      { id: 2003, age: 18, name: '里斯' },
+      { id: 2003, age: 18, name: '王五' },
+      { id: 2003, age: 18, name: '赵六' }
+    ]
+  },
+  btn_add() {
+    this.setData({
+      counter: this.data.counter + 1
+    })
+  },
+  sub_add() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
+  }
+})
